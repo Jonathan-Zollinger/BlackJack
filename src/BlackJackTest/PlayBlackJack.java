9d58c76361f6f922bd51dfab3bd1972bc1a039a4
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2020-11-17 20:20:32 -0700

    gets bjhand to cooporate with handtest.run, editcs suit symbols

--- src/BlackJackTest/PlayBlackJack.java
+++ src/BlackJackTest/PlayBlackJack.java
@@ -1,12 +1,14 @@
 package BlackJackTest;
 
 import BlackJack.BJDeck;
+import BlackJack.BJHand;
 
 public class PlayBlackJack {
     public static void main(String[] args) {
         //BJCard card = new BJCard(4,3);
         //CardTest.run(card);
     	BJDeck bjDeck = new BJDeck();
-    	DeckTest.run(bjDeck);
+        BJHand bjHand = new BJHand();
+    	HandTest.run(bjDeck,bjHand);
     }
 }
