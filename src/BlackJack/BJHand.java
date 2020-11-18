6519cad495972a69b79f0a459a78caadbbb4ad75
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2020-11-17 18:07:10 -0700

    removes .class files; adds PHand interface and BJHand.java

--- src/BlackJack/BJHand.java
+++ src/BlackJack/BJHand.java
@@ -1,4 +1,30 @@
 package BlackJack;
 
-public class BJHand {
+import BlackJackBase.*;
+
+public class BJHand implements PHand {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void addCard(PCard card) {
+
+    }
+
+    @Override
+    public PCard getCard(int index) {
+        return null;
+    }
+
+    @Override
+    public PCard removeCard(int index) {
+        return null;
+    }
+
+    @Override
+    public int getValue() {
+        return 0;
+    }
 }
